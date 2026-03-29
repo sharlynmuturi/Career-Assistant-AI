b909344384baa8e913632fecff671d53d080c334
sharlynmuturi <sharlynnmuturi@gmail.com>
2026-03-29 18:40:02 +0300

    scraped receipt project

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ with st.spinner("Extracting job data and running analysis..."):
             "pytorch", "tensorflow", "prophet", "llms", "rag", "langchain", "hugging face", "huggingface", "openai", "groq", "chromadb", "prompt engineering", "llama",
             "spark", "databricks", "mysql database", "sqlite", "mysql", "postgresql", "sql server", "ssms", "access", "mongodb",
             "tableau", "power bi", "excel", "a/b testing", "mlflow", "dvc", "regex", "ai", "tesseract", "ocr", "layoutlm", "nlp", "ai agents", "document ai",
-            "machine learning", "statistics", "statistical modelling", "statistical methods", "statistical programming", "analysis", "analytics", "prediction", "forecasting", "fraud detection", "cnns",
+            "machine learning", "deep learning", "statistics", "statistical modelling", "statistical methods", "statistical programming", "analysis", "analytics", "prediction", "forecasting", "fraud detection", "cnns",
             "shap", "survival analysis", "customer churn", "recommender systems", "nltk", "web scraping", "visualization", "data warehouse", "etl", "data transformation"
             ]
     
